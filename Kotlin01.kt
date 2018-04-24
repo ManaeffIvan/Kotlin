eb31fd5343babba75ded1c5f47e0817113914e73
ManaeffIvan <manaeff.ivan@gmail.com>
2018-04-24 17:09:26 +0500

    Supporting spaces

--- Kotlin01.kt
+++ Kotlin01.kt
@@ -1,60 +1,74 @@
-﻿var myCommand: List<String> = listOf()
+var myCommand: String = ""
+var myKey: String = ""
+var myVal: String = ""
 var myMap: MutableMap<String, String> = mutableMapOf()
 
 fun mySearch(myFunc: String){
-    if (myCommand.size < 2)
-        println("В команде $myFunc отсутствуют необходимые параметры!")
-    else {
-        val tmpMap = myMap.filter { it.key.contains(if (myCommand[1] == "*") "" else if (myCommand[1] == "\\*") "*" else myCommand[1]) and
-                (if (myCommand.size > 2) it.value.contains(myCommand[2]) else true)}
-        if (tmpMap.isNotEmpty()){
-            println("По заданным параметрам поиска в базе данных обнаружены " + (if (myFunc == "удаления") "и удалены " else "") + "записи:")
-            for (prnItem in tmpMap) {
-                println(prnItem.key + " " + prnItem.value)
-                if (myFunc == "удаления")
-                    myMap.remove(prnItem.key)
-            }
+    val tmpMap: Map<String, String>
+
+    if (myFunc == "d")
+        tmpMap = myMap.filter { (it.key == myKey) and (it.value == myVal)}
+    else
+        tmpMap = myMap.filter { it.key.contains(myKey) and it.value.contains(myVal)}
+
+    if (tmpMap.isNotEmpty()){
+        println("По заданным параметрам поиска в базе данных обнаружены " + (if (myFunc.contains("d")) "и удалены " else "") + "записи:")
+        for (prnItem in tmpMap) {
+            println("\"" + prnItem.key + "\" \"" + prnItem.value + "\"")
+            if (myFunc.contains("d"))
+                myMap.remove(prnItem.key)
         }
-        else
-            println("По заданным параметрам поиска в базе данных записей не обнаружено.")
     }
+    else
+        println("По заданным параметрам поиска в базе данных записей не обнаружено.")
 }
+
+fun inputValues(toDo: String){
+    val strInputKey = "Введите ключ"
+    val strInputValue = "Введите значение"
+    val strInputEmptyKey = " или оставьте это поле пустым для обработки всех ключей"
+    val strInputEmptyValue = " или оставьте это поле пустым для обработки всех значений"
+
+    println("$strInputKey" + (if (toDo == "d") strInputEmptyKey else ""))
+    myKey = readLine()!!
+    println("$strInputValue" + (if (toDo == "d") strInputEmptyValue else ""))
+    myVal = readLine()!!
+}
+
 fun main(args: Array<String>) {
+    val strInputCommand = "Введите команду (add|find|del|fragdel):"
+    val strHelp = "Допустимые команды: add - добавление пары, del - удаление пары, fragdel - удаление пары по части ключа и/или значения" +
+            ", find - поиск пары\nПримеры ввода команд:\n\tfind (с пустыми полями ключа и значения) - вывод всей базы данных\n\tdel (с " +
+            "пустыми полями ключа и значения) - удаление пары с пустым ключом и пустым значением\n\tfragdel (с пустыми полями ключа и " +
+            "значения) - удаление всей базы данных\nВ поиске допустимо использовать ключ или значение не целиком, а любую их часть."
 
-    val strInput = "Введите строку в формате: \"команда ключ значение\""
-    val strHelp = "Формат ввода: command key value\nДопустимые команды: add - добавление пары, del - удаление пары," +
-            " find - поиск пары\nПри отсутствии ключа в командах del или find, вместо него необходимо поставить символ \"*\"\n" +
-            "В случае, когда ключом является символ звездочки, для поиска или удаления пары с таким\n\tключом, в поле ключ " +
-            "звездочку необходимо экранировать слэшем: \"\\*\"" +
-            "\nПримеры ввода команд:\n\tadd key01 value01\n\tdel key01\n\tdel * value01\n\tdel key01 value01" +
-            "\n\tfind key01\n\tfind * value01\n\tfind key01 value01\n\tfind * - вывод всей базы данных\nДопустимо использовать" +
-            " ключ или значение не целиком, а любую их часть. В этом \n\tслучае операция будет произведена со всеми элементами, " +
-            "соответствующими заданному критерию отбора."
     println(strHelp)
-
     var myLoop = true
     while (myLoop){
-        println("\n$strInput")
-        myCommand = readLine()!!.split(" +".toRegex())
-        when (myCommand[0].toLowerCase()){
+        println("\n$strInputCommand")
+        myCommand = readLine()!!.trim().toLowerCase()
+        when (myCommand){
             "exit", "x", "q", "break", "quit" -> myLoop = false
-            "add", "put", "p" -> {
-                if (myCommand.size < 2)
-                    println("В команде добавления отсутствует ключ!")
-                else {
-                    if (myMap.filter { it.key.contains(myCommand[1])}.isNotEmpty()){
-                        println("В паре с ключом " + myCommand[1] + " значение изменено на " + (if (myCommand.size > 2) myCommand[2]
-                        else "\"\""))
-                    }
-                    else {
-                        println("Пара с ключом " + myCommand[1] + " и значением " + (if (myCommand.size > 2) myCommand[2]
-                        else "\"\"") + " успешно добавлена")
-                    }
-                    myMap.put(myCommand[1], if (myCommand.size > 2) myCommand[2] else "")
-                }
+            "add", "put", "p", "a" -> {
+                inputValues("a")
+                if (myMap.filter { it.key == myKey}.isNotEmpty())
+                    println("В паре с ключом \"$myKey\" значение изменено на \"$myVal\"")
+                else
+                    println("Пара с ключом \"$myKey\" и значением \"$myVal\" успешно добавлена")
+                myMap.put(myKey, myVal)
             }
-            "del", "delete", "d" ->  mySearch("удаления")
-            "find", "search", "f" -> mySearch("поиска")
+            "del", "delete", "d" ->  {
+                inputValues("d")
+                mySearch("d")
+            }
+            "find", "search", "f" -> {
+                inputValues("d")
+                mySearch("f")
+            }
+            "fd", "fragdelete", "fragdel" -> {
+                inputValues("d")
+                mySearch("fd")
+            }
             else -> println(strHelp)
         }
     }
